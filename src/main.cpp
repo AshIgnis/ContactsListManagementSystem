4b1dbe20ebe16da90ce165a6103ca83aa049d5d0
Lucas Qu <2267973803@qq.com>
2025-02-26 11:23:38 +0800

    初步实现联系人管理功能，添加菜单循环和用户输入处理，优化菜单显示格式

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,39 @@
 
 using namespace std;
 int main(){
-    showMenu();//菜单调用
+    int select = 0;
+    while(1){
+        showMenu();//菜单调用
+        cout << "请输入您的选择：";
+        cin >> select;
+        switch(select){
+            case 1: //添加联系人
+                
+                break;
+            case 2: //显示联系人
+                
+                break;
+            case 3: //删除联系人
+                
+                break;
+            case 4: //查找联系人
+                
+                break;
+            case 5: // 修改联系人
+                
+                break;
+            case 6: //清空联系人
+                
+                break;
+            case 0: //退出通讯录
+                cout << "欢迎下次使用" << endl;
+                system("pause");
+                return 0;
+                break;
+            default:
+                break;
+        }
+    }
 
     system("pause");
     return 0;
